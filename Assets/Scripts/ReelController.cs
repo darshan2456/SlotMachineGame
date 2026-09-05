026d1cd287b494e861e14a7e6e88e112872df6b1
darshan <darshanmparekh245624@gmail.com>
2026-09-05 18:31:59 +0530

    UI: configured ReelController.cs to move symbols up and down

--- Assets/Scripts/ReelController.cs
+++ Assets/Scripts/ReelController.cs
@@ -1,16 +1,61 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class ReelController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Transform Symbols;
+    [SerializeField] private float spinSpeed = 8f;
+    private bool isSpinning = false;
+
+    private Transform[] symbolObjects;
+
+    public void Start()
     {
-        
+        symbolObjects = new Transform[Symbols.childCount];
+
+        for (int i = 0; i < Symbols.childCount; i++)
+        {
+            symbolObjects[i] = Symbols.GetChild(i);
+        }
+
+        startSpin();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void startSpin()
     {
-        
+        isSpinning = true;
+    }
+
+    public void stopSpin()
+    {
+        isSpinning = false;
+    }
+
+    private void Update()
+    {
+        if (!isSpinning)
+        {
+            return;
+        }
+
+        MoveSymbols();
+
+    }
+
+
+
+    private void MoveSymbols()
+    {
+        foreach (Transform symbol in symbolObjects)
+        {
+            symbol.localPosition +=
+                Vector3.down * spinSpeed * Time.deltaTime;
+
+            if (symbol.localPosition.y < -2f)
+            {
+                Vector3 pos = symbol.localPosition;
+                pos.y += 4f;
+                symbol.localPosition = pos;
+            }
+        }
     }
 }
